56b92c954dd1288f00efdb088b83ff63ab2d5140
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-23 00:39:04 -0500

    adding delete, disable, and create methods

--- complete/src/main/java/com/yern/service/secrets/SecretManager.java
+++ complete/src/main/java/com/yern/service/secrets/SecretManager.java
@@ -1,6 +1,5 @@
 package com.yern.service.secrets;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -13,8 +12,33 @@ public interface SecretManager {
      * If the secret is found, it's returned from the method.
      *
      * @param secretName The full name of the secret.
-     * @param version The version of the secret. If the value is empty, the latest version will be returned
+     * @param version The version of the secret. If the value is empty, the latest version will be returned.
      * @throws SecretNotFoundException If that secret is not found, a SecretNotFoundException is thrown.
     */
     public String get(String secretName, Optional<String> version) throws SecretNotFoundException;
+
+    /**
+     * Creates a new secret not attached to any other, existing version.
+     *
+     * @param secretName The full name of the secret.
+     * @param secret The actual value to be stored under the `secretName`
+     * @throws SecretAlreadyExistException If that secret already exists. 
+    */
+
+    /**
+     * Complete removes a secret based on the provided name.
+     *
+     * @param secretName The full name of the secret.
+     * @param version The version of the secret. If the value is empty, the latest version will be used.
+     * @throws RuntimeException If the secret cannot be deleted.
+    */
+    public void delete(String secretName, Optional<String> version) throws RuntimeException;
+    
+    /**
+     * Updates the supplied secret to a state that is no longer considered valid for production use.
+     * 
+     * @param secretName The full name of the secret.
+     * @throws SecretNotFoundException If the secret cannot be found.
+    */
+    public void disable(String secretName);
 }
