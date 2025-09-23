738a83b963375a09895352c5508433aa5e0bcb97
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-23 00:23:28 -0500

    adding get method definition and doc

--- complete/src/main/java/com/yern/service/secrets/SecretManager.java
+++ complete/src/main/java/com/yern/service/secrets/SecretManager.java
@@ -1,5 +1,20 @@
 package com.yern.service.secrets;
 
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Defines the subset of methods that all cloud secret providers should implement.
+ */
 public interface SecretManager {
-    
+    /**
+     * Searches for a secret matching the `secretName` and `version` params.
+     * If that secret is not found, a SecretNotFoundException is thrown.
+     * If the secret is found, it's returned from the method.
+     *
+     * @param secretName The full name of the secret.
+     * @param version The version of the secret. If the value is empty, the latest version will be returned
+     * @throws SecretNotFoundException If that secret is not found, a SecretNotFoundException is thrown.
+    */
+    public String get(String secretName, Optional<String> version) throws SecretNotFoundException;
 }
