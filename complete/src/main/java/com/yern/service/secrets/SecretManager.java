d2c54ca2ce8ad3bd933dca857de6a3f1e67717c1
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-24 11:27:07 -0500

    adding required IOExceptions for instantiating secret manager client

--- complete/src/main/java/com/yern/service/secrets/SecretManager.java
+++ complete/src/main/java/com/yern/service/secrets/SecretManager.java
@@ -3,6 +3,7 @@ package com.yern.service.secrets;
 import com.yern.service.secrets.SecretNotFoundException;
 import com.yern.service.secrets.SecretAlreadyExistsException;
 
+import java.io.IOException;
 import java.util.Optional;
 
 /**
@@ -18,7 +19,7 @@ public interface SecretManager {
      * @param version The version of the secret. If the value is empty, the latest version will be returned.
      * @throws SecretNotFoundException If that secret is not found, a SecretNotFoundException is thrown.
     */
-    public String get(String secretName, Optional<String> version) throws SecretNotFoundException;
+    public String get(String secretName, Optional<String> version) throws IOException, SecretNotFoundException;
 
     /**
      * Creates a new secret not attached to any other, existing version.
@@ -27,7 +28,7 @@ public interface SecretManager {
      * @param secret The actual value to be stored under the `secretName`
      * @throws SecretAlreadyExistsException If that secret already exists. 
     */
-    public void create(String secretName, String secret) throws SecretAlreadyExistsException;
+    public void create(String secretName, String secret) throws IOException, SecretAlreadyExistsException;
 
     /**
      * Complete removes a secret based on the provided name.
@@ -36,7 +37,7 @@ public interface SecretManager {
      * @param version The version of the secret. If the value is empty, the latest version will be used.
      * @throws RuntimeException If the secret cannot be deleted.
     */
-    public void delete(String secretName, Optional<String> version) throws RuntimeException;
+    public void delete(String secretName, Optional<String> version) throws IOException, RuntimeException;
     
     /**
      * Updates the supplied secret to a state that is no longer considered valid for production use.
@@ -44,5 +45,5 @@ public interface SecretManager {
      * @param secretName The full name of the secret.
      * @throws RuntimeException If the secret cannot be found.
     */
-    public void disable(String secretName) throws RuntimeException;
+    public void disable(String secretName) throws IOException, RuntimeException;
 }
