799ff584f9e5cd9d7f951a697eee5ace24cfb842
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-25 20:49:14 -0500

    adding secret already exists exception

--- complete/src/main/java/com/yern/service/secrets/SecretAlreadyExistsException.java
+++ complete/src/main/java/com/yern/service/secrets/SecretAlreadyExistsException.java
@@ -1,6 +1,12 @@
 package com.yern.service.secrets;
 
 public class SecretAlreadyExistsException extends RuntimeException {
+    private static final String DEFAULT_MESSAGE = "Secret already exists: ";
+
+    public static String getFormattedMessage(String secretName) {
+        return (DEFAULT_MESSAGE + secretName);
+    }
+    
     public SecretAlreadyExistsException(String message) {
         super(message);
     }
