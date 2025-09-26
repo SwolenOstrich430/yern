008f2ff0703ef2905c56e4a5a793be1224d0ecd8
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-26 13:38:23 -0500

    adding initial interface for Storage Providers

--- complete/src/test/java/com/yern/service/storage/StorageProvider.java
+++ complete/src/test/java/com/yern/service/storage/StorageProvider.java
@@ -1,5 +1,17 @@
 package com.yern.service.storage;
 
-public interface StorageProvider {
+import java.io.File;
+import java.util.List;
 
+public interface StorageProvider {
+    public boolean fileExists(String path);
+    public void createFolder(String path);
+    public List<String> listFolders(String folderPath);
+    public void deleteFolder(String path);
+    public void uploadFile(String localPath, String targetPath);
+    public File downloadFile(String localPath, String targetPath);
+    public void updateFile(String path);
+    public void deleteFile(String path); 
+    public void copyFile(String currentPath, String targetPath);
+    public void moveFile(String curreString, String targetPath);
 }
