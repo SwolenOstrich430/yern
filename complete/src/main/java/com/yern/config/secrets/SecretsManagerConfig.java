13ca434335ae2ae1e09413e99dea63dc9ee60be7
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-25 20:48:10 -0500

    adding config for secrets manager service client

---
+++ complete/src/main/java/com/yern/config/secrets/SecretsManagerConfig.java
@@ -0,0 +1,17 @@
+package com.yern.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+
+@Configuration
+public class SecretsManagerConfig {
+
+    @Bean
+    public SecretManagerServiceClient secretManagerServiceClient() throws IOException {
+        return SecretManagerServiceClient.create();
+    }
+}
