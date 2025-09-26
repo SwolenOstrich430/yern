d0f476207f4497c032b23775bbbe6a361dc81f40
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-25 20:49:42 -0500

    updating secret manager interface for differentiated secret and version methods

--- complete/src/main/java/com/yern/service/secrets/SecretManager.java
+++ complete/src/main/java/com/yern/service/secrets/SecretManager.java
@@ -1,9 +1,5 @@
 package com.yern.service.secrets;
 
-import com.yern.service.secrets.SecretNotFoundException;
-import com.yern.service.secrets.SecretAlreadyExistsException;
-
-import java.io.IOException;
 import java.util.Optional;
 
 /**
@@ -19,7 +15,7 @@ public interface SecretManager {
      * @param version The version of the secret. If the value is empty, the latest version will be returned.
      * @throws SecretNotFoundException If that secret is not found, a SecretNotFoundException is thrown.
     */
-    public String get(String secretName, Optional<String> version) throws IOException, SecretNotFoundException;
+    public SecretImpl getSecret(String secretName, Optional<String> version) throws SecretNotFoundException;
 
     /**
      * Creates a new secret not attached to any other, existing version.
@@ -28,22 +24,40 @@ public interface SecretManager {
      * @param secret The actual value to be stored under the `secretName`
      * @throws SecretAlreadyExistsException If that secret already exists. 
     */
-    public void create(String secretName, String secret) throws IOException, SecretAlreadyExistsException;
+    public void createSecret(String secretName, String secret) throws SecretAlreadyExistsException;
 
     /**
-     * Complete removes a secret based on the provided name.
+     * Removes a secret based on the provided name.
+     *
+     * @param secretName The full name of the secret.
+     * @throws RuntimeException If the secret cannot be deleted.
+    */
+    public void deleteSecret(String secretName);
+    
+      /**
+     * Removes a secret based on the provided name.
      *
      * @param secretName The full name of the secret.
      * @param version The version of the secret. If the value is empty, the latest version will be used.
      * @throws RuntimeException If the secret cannot be deleted.
     */
-    public void delete(String secretName, Optional<String> version) throws IOException, RuntimeException;
+    public void deleteVersion(String secretName, Optional<String> version);
     
     /**
      * Updates the supplied secret to a state that is no longer considered valid for production use.
      * 
      * @param secretName The full name of the secret.
+     * @param version The version of the secret. If the value is empty, the latest version will be used.
      * @throws RuntimeException If the secret cannot be found.
     */
-    public void disable(String secretName) throws IOException, RuntimeException;
+    public void disableVersion(String secretName, Optional<String> version);
+
+    /**
+     * Updates the supplied secret to a state that is valid for production use.
+     * 
+     * @param secretName The full name of the secret.
+     * @param version The version of the secret. If the value is empty, the latest version will be used.
+     * @throws RuntimeException If the secret cannot be found.
+    */
+    public void enableVersion(String secretName, Optional<String> version);
 }
