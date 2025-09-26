bfd1914b6215f94e1a539f7a423dff0b731f2e6e
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-26 15:03:18 -0500

    adding class definition for internal bucket with member variable, name

--- complete/src/main/java/com/yern/service/storage/BucketImpl.java
+++ complete/src/main/java/com/yern/service/storage/BucketImpl.java
@@ -1,5 +1,18 @@
 package com.yern.service.storage;
 
-public class BucketImpl {
+import com.google.cloud.storage.Bucket;
 
+import lombok.Getter;
+
+@Getter
+public class BucketImpl {
+    private String name;
+
+    public static BucketImpl from(Bucket gcsBucket) {
+        return new BucketImpl(gcsBucket.getName());
+    }
+
+    public BucketImpl(String name) {
+        this.name = name;
+    }
 }
