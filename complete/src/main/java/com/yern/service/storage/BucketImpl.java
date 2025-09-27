3a57105a3ae9f3ad7459fc5f8fb078ee826d8e61
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-27 01:56:04 -0500

    adding equals method for BucketImpl and todo to add provider as an enum

--- complete/src/main/java/com/yern/service/storage/BucketImpl.java
+++ complete/src/main/java/com/yern/service/storage/BucketImpl.java
@@ -1,10 +1,13 @@
 package com.yern.service.storage;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 import com.google.cloud.storage.Bucket;
 
 import lombok.Getter;
 
-@Getter
+// TODO: this should probably be moved to a factory 
 public class BucketImpl {
     private String name;
 
@@ -15,4 +18,18 @@ public class BucketImpl {
     public BucketImpl(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof BucketImpl)) return false;
+
+		BucketImpl bucket = (BucketImpl) o;
+
+		return Objects.equals(this.name, bucket.getName());
+	}
 }
