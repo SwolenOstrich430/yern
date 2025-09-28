05305f2e87d659cdcb038db78f4d68bdb244ad21
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-28 00:08:35 -0500

    adding jpa annotations

--- complete/src/main/java/com/yern/model/pattern/Pattern.java
+++ complete/src/main/java/com/yern/model/pattern/Pattern.java
@@ -2,7 +2,11 @@ package com.yern.model.pattern;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,10 +20,23 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Pattern {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Basic
+    @Column
     private String name;
+    
+    @Basic
+    @Column
     private String description;
+    
+    @Basic
+    @Column
     private LocalDateTime created_at;
+    
+    @Basic
+    @Column
     private LocalDateTime updated_at;
 
     
