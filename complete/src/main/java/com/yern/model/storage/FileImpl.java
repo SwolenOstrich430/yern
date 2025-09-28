aba45ca1c4b0556e30e4432f124d8c0333c90236
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-28 11:33:47 -0500

    adding member variables for FileImpl

--- complete/src/main/java/com/yern/model/storage/FileImpl.java
+++ complete/src/main/java/com/yern/model/storage/FileImpl.java
@@ -1,6 +1,24 @@
 package com.yern.model.storage;
 
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+ 
+@Entity
+@Table(name="files")
+@NoArgsConstructor
+@AllArgsConstructor
 public class FileImpl {
-
+    private Long id;
+    private StorageProviderType storageProvider;
+    private String raw_path;
+    private String formatted_path;
+    private String public_url;
+    private String etag;
+    private ProcessFileException error;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
