cd9eae37ec4229fbc5e1ad22a7ebefaad8b5013f
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-28 12:07:55 -0500

    adding hasError to FileImpl test

--- complete/src/main/java/com/yern/model/storage/FileImpl.java
+++ complete/src/main/java/com/yern/model/storage/FileImpl.java
@@ -5,12 +5,16 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
  
 @Entity
 @Table(name="files")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class FileImpl {
     private Long id;
     private StorageProviderType storageProvider;
@@ -21,4 +25,10 @@ public class FileImpl {
     private ProcessFileException error;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public boolean hasError() {
+        return (
+            getError() instanceof ProcessFileException
+        );
+    }
 }
--- complete/src/test/java/com/yern/model/storage/FileImplTest.java
+++ complete/src/test/java/com/yern/model/storage/FileImplTest.java
@@ -1,5 +1,31 @@
 package com.yern.model.storage;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class FileImplTest {
 
+    private FileImpl file; 
+    private ProcessFileException error;
+
+    @BeforeEach
+    public void setup() {
+        file = new FileImpl();
+        error = Mockito.mock(ProcessFileException.class);
+    }
+
+    @Test
+    public void hasError_returnsTrue_whenErrorIsNotNull() {
+        file.setError(error);
+        assertTrue(file.hasError());
+    }
+
+    @Test
+    public void hasError_returnsFalse_whenErrorIsNotNull() {
+        file.setError(null);
+        assertFalse(file.hasError());
+    }
 }
