bfcf62bcff0995687ca2c71bf87e044ccf708223
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-28 12:43:31 -0500

    creating file service to orchestrate the entire file processing cycle

---
+++ complete/src/main/java/com/yern/service/storage/FileService.java
@@ -0,0 +1,21 @@
+package com.yern.service.storage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yern.repository.storage.FileRepository;
+
+@Service
+public class FileService {
+    private FileRepository fileRepository;
+    private StorageProvider storageProvider;
+    private FileProcessor fileProcessor;
+
+    public FileService(
+        @Autowired FileRepository fileRepository,
+        @Autowired StorageProvider storageProvider
+    ) {
+        this.fileRepository = fileRepository;
+        this.storageProvider = storageProvider;
+    }
+}
