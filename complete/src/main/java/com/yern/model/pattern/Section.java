266678ee71ff3031d73974c90f66b5fc639e8cfa
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-28 00:21:29 -0500

    adding section model definition

--- complete/src/main/java/com/yern/model/pattern/Section.java
+++ complete/src/main/java/com/yern/model/pattern/Section.java
@@ -1,5 +1,28 @@
 package com.yern.model.pattern;
 
-public class Section {
 
+import java.time.LocalDateTime;
+
+import com.yern.model.storage.FileImpl;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity 
+@Table(name="sections")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Section {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long patternId;
+    private String name;
+    private String notes;
+    private FileImpl file;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
