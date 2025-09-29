6a377fb1d6f2821dea7a1e5d8aed05081f1c93b2
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-29 15:54:40 -0500

    splitting out is and has valid media type

--- complete/src/main/java/com/yern/service/storage/FileProcessorOrchestrator.java
+++ complete/src/main/java/com/yern/service/storage/FileProcessorOrchestrator.java
@@ -3,12 +3,10 @@ package com.yern.service.storage;
 import java.io.IOException;
 import java.nio.file.Path;
 
-import org.springframework.http.MediaType;
-
 import com.yern.exceptions.NotFoundException;
 
 public interface FileProcessorOrchestrator extends FileProcessor {
     FileProcessor getProvider(Path filePath) throws NotUniqueException, IOException, NotFoundException;
-    public boolean hasValidMediaType(MediaType mediaType);
+    public boolean hasValidMediaType(Path filePath) throws IOException;
 }
 
