3376acba19ded794506adab4b0f799ed6110385c
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-30 10:26:47 -0500

    planning out integration test

--- complete/src/test/java/com/yern/model/storage/PDFProcessorIntegrationTest.java
+++ complete/src/test/java/com/yern/model/storage/PDFProcessorIntegrationTest.java
@@ -1,5 +1,10 @@
 package com.yern.model.storage;
 
 public class PDFProcessorIntegrationTest {
-
+    /**
+     * 1. create a pdf document with pdf box 
+     * 2. add a document id 
+     * 3. add catalog metadata 
+     * 4. add PDActionJavaScript to getDocumentCatalog().getOpenAction()
+     */
 }
