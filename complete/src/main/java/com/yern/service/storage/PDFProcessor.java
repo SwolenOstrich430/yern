184075fc579edd1d17043f77f37a5dc98ce5ab31
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-09-30 10:20:49 -0500

    adding pdf processing and todo for an integration test with the more complicated cleaning

--- complete/src/main/java/com/yern/service/storage/PDFProcessor.java
+++ complete/src/main/java/com/yern/service/storage/PDFProcessor.java
@@ -2,8 +2,20 @@ package com.yern.service.storage;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDMetadata;
+import org.apache.pdfbox.pdmodel.interactive.action.PDActionJavaScript;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.springframework.http.MediaType;
 
 import lombok.NoArgsConstructor;
@@ -18,26 +30,70 @@ public class PDFProcessor implements FileProcessor {
     public void processFile(
         Path filePath
     ) throws IOException {
-        removeMetaData();
-        removeSensitiveContent();
-        flattenFile();
-        compressFile();
+        try (PDDocument document = Loader.loadPDF(filePath.toFile())) {
+            removeMetaData(document);
+            removeJavaScript(document);
+            removeEmbeddedFiles(document);
+            flattenFormFields(document);
+            // compression should be happening here by default 
+            // could enable xref 
+            document.save(filePath.toString());
+        } 
     }
 
-    public void removeMetaData() {
+    public void removeMetaData(
+        PDDocument document
+    ) throws IOException {
+        COSArray documentID = document.getDocument().getDocumentID();
+        if (documentID != null) {
+            documentID.clear();
+        }
 
+        PDDocumentCatalog catalog = document.getDocumentCatalog();
+        PDMetadata metadata = catalog.getMetadata();
+        if (metadata != null) {
+            catalog.setMetadata(null); 
+        }
     }
 
-    public void removeSensitiveContent() {
+    public void removeJavaScript(PDDocument document) throws IOException {
+        // Remove document-level JavaScript
+        if (document.getDocumentCatalog().getOpenAction() instanceof PDActionJavaScript) {
+            document.getDocumentCatalog().setOpenAction(null);
+        }
 
+        List<PDAnnotation> annotations = new ArrayList<>();
+        for (PDPage page : document.getPages()) {;
+            if (page.getAnnotations() != null) {
+                annotations.addAll(page.getAnnotations());
+            }
+        }
+        // Remove JavaScript from annotations (e.g., form fields)
+        PDAnnotationWidget widget;
+        for (PDAnnotation annotation : annotations) {
+            if (annotation instanceof PDAnnotationWidget) {
+                widget = (PDAnnotationWidget) annotation;
+                if (widget.getActions() != null && widget.getActions().getCOSObject().containsKey(COSName.JS)) {
+                    widget.getActions().getCOSObject().removeItem(COSName.JS);
+                }
+            }
+        }
     }
 
-    public void flattenFile() {
-
+    public void removeEmbeddedFiles(PDDocument document) throws IOException {
+        if (document.getDocumentCatalog().getNames() != null &&
+            document.getDocumentCatalog().getNames().getEmbeddedFiles() != null) {
+            document.getDocumentCatalog().getNames().getEmbeddedFiles().getNames().clear();
+        }
     }
 
-    public void compressFile() {
-
+    public void flattenFormFields(PDDocument document) throws IOException {
+        PDAcroForm acroForm = document.getDocumentCatalog().getAcroForm();
+        
+        if (acroForm != null) {
+            acroForm.flatten(); 
+            document.getDocumentCatalog().setAcroForm(null); // Optionally remove the AcroForm entirely
+        }
     }
 
     @Override
