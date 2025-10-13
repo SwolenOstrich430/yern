d05c46727907aa75a5ff4099ecbb24177c6414b1
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-13 15:51:01 -0500

    adding access control repository

--- complete/src/main/java/com/yern/service/storage/file/access/FileAccessControlService.java
+++ complete/src/main/java/com/yern/service/storage/file/access/FileAccessControlService.java
@@ -1,6 +1,19 @@
 package com.yern.service.storage.file.access;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yern.repository.storage.FileAccessControlRespository;
+
 @Service
 public class FileAccessControlService {
+    private FileAccessControlRespository accessRepository;
 
+    public FileAccessControlService(
+        @Autowired FileAccessControlRespository accessRepository
+    ) {
+        this.accessRepository = accessRepository;
+    }
+
+    
 }
