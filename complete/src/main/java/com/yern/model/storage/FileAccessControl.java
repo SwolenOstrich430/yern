e78a89167a142ea0f46cd6ea05d0a6d69e8490ca
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-14 12:30:30 -0500

    adding hashCode, equals, and from for FileAccessControl

--- complete/src/main/java/com/yern/model/storage/FileAccessControl.java
+++ complete/src/main/java/com/yern/model/storage/FileAccessControl.java
@@ -6,25 +6,45 @@ import org.springframework.cglib.core.Local;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity 
 @Table(name = "file_access_controls")
+@Getter
+@Setter
 @NoArgsConstructor
 public class FileAccessControl {
-    @Id 
+
+    // TODO: add unit test
+    public static FileAccessControl from(
+        Long userId, 
+        Long fileId, 
+        Long roleId
+    ) {
+        FileAccessControl accessControl = new FileAccessControl();
+        accessControl.setUserId(userId);
+        accessControl.setFileId(fileId);
+        accessControl.setRoleId(roleId);
+
+        return accessControl;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column 
     private Long userId;
 
-    @Id 
     @Column 
     private Long roleId;
 
-    @Id 
     @Column 
     private Long fileId;
 
@@ -49,5 +69,30 @@ public class FileAccessControl {
     @PreUpdate 
     public void preUpdate() {
         updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; 
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        FileAccessControl other = (FileAccessControl) obj; 
+
+        return (
+            this.roleId == other.getRoleId() && 
+            this.fileId == other.getFileId() && 
+            this.userId == other.getUserId()
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return (
+            31 * (
+                Long.hashCode(roleId) +
+                Long.hashCode(fileId) + 
+                Long.hashCode(userId)
+            )
+        );
     }
 }
