c951123cd6261e8d51802ec5bca01d4ca6da3d69
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-14 12:29:28 -0500

    adding findByFileId in file access control repo

--- complete/src/main/java/com/yern/repository/storage/FileAccessControlRespository.java
+++ complete/src/main/java/com/yern/repository/storage/FileAccessControlRespository.java
@@ -1,9 +1,13 @@
 package com.yern.repository.storage;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.yern.model.storage.FileAccessControl;
 
-public interface FileAccessControlRespository extends JpaRepository<Long, FileAccessControl> {
-
+@Repository
+public interface FileAccessControlRespository extends JpaRepository<FileAccessControl, Long> {
+    List<FileAccessControl> findByFileId(Long fileId);
 }
