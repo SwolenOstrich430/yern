6e36ffd15c336651e226d5e5c1b7c57bac84e0ec
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-14 12:29:41 -0500

    adding basic outline of role class

---
+++ complete/src/main/java/com/yern/model/security/Role.java
@@ -0,0 +1,39 @@
+package com.yern.model.security;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter 
+@NoArgsConstructor
+public class Role {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column 
+    private String name; 
+
+    @Column 
+    private String identifier; 
+    
+    @OneToMany(
+        mappedBy = "role", 
+        cascade = CascadeType.ALL, 
+        orphanRemoval = true
+    )
+    private Set<RolePermission> permissions;
+}
