430daf97c6c529edd8244dd628b3e3a356675f13
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-15 13:36:32 -0500

    adding tests and implementation for validateRolePermissions

--- complete/src/main/java/com/yern/service/security/authorization/RoleService.java
+++ complete/src/main/java/com/yern/service/security/authorization/RoleService.java
@@ -1,13 +1,16 @@
 package com.yern.service.security.authorization;
 
 import com.yern.model.security.ResourceType;
+import com.yern.model.security.authorization.Permission;
 import com.yern.model.security.authorization.Role;
+import com.yern.model.security.authorization.RoleType;
 import com.yern.repository.security.authorization.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class RoleService {
@@ -41,11 +44,13 @@ public class RoleService {
     }
 
     /**
+     * TODO: move this to Role?
+     * 
      * @param **Role** role - a role with or without RolePermissions set
      * 
      * **Rules for Standard Role Types** 
      * * OWNER: 
-     *      * required permissions: 'OWN', 'READ', 'DELETE', and 'AUTHORIZE'
+     *      * required permissions: 'OWN', 'UPDATE', 'READ', 'DELETE', and 'AUTHORIZE'
      *      * restricted permission: N/A
      * * EDITOR: 
      *      * required permissions: 'UPDATE' and 'READ'
@@ -54,11 +59,42 @@ public class RoleService {
      *      * requires 'READ' roles 
      *      * restricts 'UPDATE', 'OWN', 'AUTHORIZE'
      * 
+     * All of the baove should be enforced in the database. Adding this 
+     * here as an extra validtor since this should be enabled before 
+     * we can expect the app to actually be able to run. 
+     * 
      * @return null if the role's permissions are valid 
      * 
-     * @throws InvalidRoleException if role's permission aren't valid 
+     * @throws AssertionError if role's permission aren't valid
+     * @throws AssertionError if role is null or its permissions are empty
     */
     public void validateRolePermissions(Role role) {
-        return;
+        assert(role != null);
+        Set<Permission> permissions = role.getRawPermissions();
+        assert(permissions != null);
+        assert(!permissions.isEmpty());
+
+        RoleType type = role.getType();
+
+        if (type == RoleType.OWNER) {
+            assert(permissions.contains(Permission.OWN));
+            assert(permissions.contains(Permission.AUTHORIZE));
+            assert(permissions.contains(Permission.DELETE));
+        } else {
+            assert(!permissions.contains(Permission.OWN));
+            assert(!permissions.contains(Permission.DELETE));
+            assert(!permissions.contains(Permission.AUTHORIZE));
+        }
+
+        if (type == RoleType.EDITOR || type == RoleType.OWNER) {
+            assert(permissions.contains(Permission.UPDATE));
+            assert(permissions.contains(Permission.READ));
+        } else {
+            assert(!permissions.contains(Permission.UPDATE));
+        }
+
+        if (type == RoleType.READER) {
+            assert(permissions.contains(Permission.READ));
+        }
     }
 }
