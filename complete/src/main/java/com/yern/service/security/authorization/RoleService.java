0c3a8fde215a6252281abacedeccf4094d2d8930
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-15 10:30:15 -0500

    uncommenting role service now that we need it

--- complete/src/main/java/com/yern/service/security/authorization/RoleService.java
+++ complete/src/main/java/com/yern/service/security/authorization/RoleService.java
@@ -1,27 +1,27 @@
-//package com.yern.service;
-//
-//import com.yern.model.user.Role;
-//import com.yern.repository.RoleRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class RoleService {
-//
-//    @Autowired
-//    private RoleRepository roleRepository;
-//
-//    public Role getRoleById(Long id) {
-//        return roleRepository.getRoleById(id);
-//    }
-//
-//    public List<Role> getAllRoles() {
-//        return roleRepository.findAll();
-//    }
-//
-//    public Role createRole(Role role) {
-//        return roleRepository.save(role);
-//    }
-//}
+package com.yern.service;
+
+import com.yern.model.user.Role;
+import com.yern.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleService {
+
+   @Autowired
+   private RoleRepository roleRepository;
+
+   public Role getRoleById(Long id) {
+       return roleRepository.getRoleById(id);
+   }
+
+   public List<Role> getAllRoles() {
+       return roleRepository.findAll();
+   }
+
+   public Role createRole(Role role) {
+       return roleRepository.save(role);
+   }
+}
