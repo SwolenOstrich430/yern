b98243867a3bada54da0fd7a147ee9a17c04e932
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-16 13:59:12 -0500

    adding user schema for migrations

--- complete/src/main/resources/db/changelog/security/authorization/initial-tables.sql
+++ complete/src/main/resources/db/changelog/security/authorization/initial-tables.sql
@@ -2,5 +2,19 @@ create table if not exists yern.roles (
     "id" bigserial primary key,
     "resource" yern.resource_type unique not null,
     "type" yern.role_type unique not null,
-    "display_name" varchar(100) unique not null
+    "display_name" varchar(100) unique not null,
+    "audit_timestamps" audit_timestamps
+);
+
+create table if not exists yern.roles_permissions (
+    "role_id" bigint not null,
+    "permission" yern.permission not null,
+    "audit_timestamps" audit_timestamps,
+    primary key("role_id", "permission")
+);
+
+create table if not exists yern.user_roles (
+    "user_id" bigint not null,
+    "role_id" bigint not null,
+    "audit_timestamps" audit_timestamps
 );
--- complete/src/main/resources/db/changelog/user/initial-tables.sql
+++ complete/src/main/resources/db/changelog/user/initial-tables.sql
@@ -3,5 +3,5 @@ CREATE TABLE yern.users (
     "email" text unique not null,
     "first_name" varchar(20) NOT NULL,
     "last_name" varchar(20) NOT NULL,
-    "audit_timestamps" yern.audit_timestamps
+    "audit_timestamps" audit_timestamps
 );
