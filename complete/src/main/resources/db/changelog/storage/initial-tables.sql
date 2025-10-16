6eac962031514a3778cb225d8bb1e4ade0a1efeb
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-16 14:09:02 -0500

    adding initial file tables

--- complete/src/main/resources/db/changelog/storage/initial-tables.sql
+++ complete/src/main/resources/db/changelog/storage/initial-tables.sql
@@ -1,23 +1,18 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'yern.storage_provider') THEN
-        CREATE TYPE storage_provider AS ENUM ('GCS');
-    END IF;
-END
-$$;
-
 create table if not exists yern.files (
     "id" bigserial primary key,
-    "storage_provider" yern.storage_provider NOT NULL,
+    "storage_provider" yern.storage_provider not null,
     "raw_path" text not null,
     "formatted_path" text,
     "public_url" text,
     "etag" text,
     "error" jsonb,
-    "created_at" timestamp not null default cast(
-        now() as timestamp without time zone
-    ),
-    "updated_at" timestamp not null default cast(
-        now() as timestamp without time zone
-    )
+    "audit_timestamps" audit_timestamps
+);
+
+create table if not exists yern.file_access_control (
+    "id" bigserial primary key,
+    "user_id" bigint not null references users(id) on delete cascade,
+    "file_id" bigint not null references files(id) on delete cascade,
+    "role_id" bigint not null references roles(id) on delete cascade,
+    "audit_timestamps" audit_timestamps
 );
