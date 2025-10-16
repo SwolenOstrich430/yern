4ebf3a42a1120120b45bd86a6845f4960f2b5abd
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-16 13:33:34 -0500

    adding enum factory function

---
+++ complete/src/main/resources/db/changelog/common/helper/initial-factory-functions.sql
@@ -0,0 +1,23 @@
+create or replace function create_enum(
+    enum_name TEXT,
+    enum_values TEXT[]
+)
+returns void AS $$
+declare
+    value_list text;
+    single_value text;
+begin
+    -- Construct the value list for CREATE TYPE
+    value_list := '';
+    for i in 1..array_length(enum_values, 1) loop
+        if i > 1 then
+            value_list := value_list || ', ';
+        end if;
+        value_list := value_list || quote_literal(enum_values[i]);
+    end loop;
+
+    execute format('create type %I AS ENUM (%s)', enum_name, value_list);
+    RAISE NOTICE 'Enum type % created with initial values.', enum_name;
+
+end;
+$$ language plpgsql;
