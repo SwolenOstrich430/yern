1f45723066dcb022ab6c2374c00a82a0a1d6674c
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-16 12:51:09 -0500

    adding users and initial audit types

---
+++ complete/src/main/resources/db/changelog/user/initial-tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE yern.users (
+    "id" bigserial,
+    "email" text unique not null,
+    "first_name" varchar(20) NOT NULL,
+    "last_name" varchar(20) NOT NULL,
+    "audit_timestamps" yern.audit_timestamps
+);
