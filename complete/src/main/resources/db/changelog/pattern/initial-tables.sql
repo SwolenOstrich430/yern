c67d2fdb7a88db8d336d57f9640116c21900b31e
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-16 15:25:50 -0500

    adding uuid extension and pattern definitions

---
+++ complete/src/main/resources/db/changelog/pattern/initial-tables.sql
@@ -0,0 +1,51 @@
+create table if not exists yern.patterns(
+    "id" bigserial primary key,
+    "name" varchar(100) not null,
+    "description" varchar(255) not null,
+    "audit_timestamps" audit_timestamps
+);
+
+create table if not exists yern.counters(
+    "id" bigserial primary key,
+    "value" bigint not null default 0,
+    "is_dirty" bool not null default false,
+    "last_reset_at" timestamp,
+    "audit_timestamps" audit_timestamps
+);
+
+create table if not exists yern.counter_logs(
+    "id" uuid primary key default gen_random_uuid(),
+    "counter_id" bigint not null,
+    "external_id" text unique not null,
+    "value" integer not null,
+    "created_at" timestamp not null default now()
+);
+
+create table if not exists yern.sections(
+    "id" bigserial primary key,
+    "pattern_id" 
+        bigint not null 
+        references yern.patterns(id) 
+        on delete cascade,
+    "name" varchar(100) not null,
+    "description" varchar(255),
+    "notes" text,
+    "sequence" integer not null,
+    "file_id" 
+        bigint 
+        references yern.files(id) 
+        on delete cascade,
+    "counter_id" 
+        bigint 
+        references yern.counters(id) 
+        on delete cascade,
+    "audit_timestamps" audit_timestamps
+);
+
+create table if not exists yern.counters(
+    "id" bigserial primary key,
+    "value" bigint not null default 0,
+    "is_dirty" boolean not null default false,
+    "last_reset_at" timestamp without time zone,
+    "audit_timestamps" audit_timestamps
+);
