9b1c71ece22f059c66225f7b20aae61761be4a4b
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-10-20 10:00:38 -0500

    moving role permission validation to role class

--- complete/src/main/java/com/yern/model/security/authorization/Role.java
+++ complete/src/main/java/com/yern/model/security/authorization/Role.java
@@ -50,8 +50,93 @@ public class Role {
     )
     private Set<RolePermission> permissions;
 
+    public Role(
+        Long id,
+        String name,
+        RoleType type,
+        Set<RolePermission> permissions
+    ) {
+        setId(id);
+        setName(name);
+        setType(type);
+        setPermissions(permissions);
+    }
+
+    public void setRawPermissions(Set<Permission> permissions) {
+        validatePermissions(permissions);
+        
+        this.permissions = 
+            permissions
+                .stream()
+                .map(permission -> new RolePermission(this, permission))
+                .collect(Collectors.toSet());
+    }
+
+    public void setPermissions(Set<RolePermission> permissions) {
+        Set<Permission> rawPermissions = getRawPermissions(permissions);
+        setRawPermissions(rawPermissions);
+    }
+
+    public boolean isType(RoleType type) {
+        assert(type != null);
+        return this.type == type;
+    }
+
+    public boolean controlsResource(ResourceType resource) {
+        assert(resource != null);
+        return this.resource == resource;
+    }
+
+    public void validate(RoleType type, ResourceType resource) {
+        assert(isType(type));
+        assert(controlsResource(resource));
+        validatePermissions();
+    }
+
+    public void validatePermissions() {
+        validatePermissionsRaw(
+            getRawPermissions(),
+            getType()
+        );
+    }
+
+    public void validatePermissions(Set<Permission> permission) {
+        validatePermissionsRaw(permission, type);
+    }
+    
+    private void validatePermissionsRaw(Set<Permission> permissions, RoleType roleType) {
+        assert(permissions != null);
+        assert(!permissions.isEmpty());
+        assert(roleType != null);
+
+        if (roleType == RoleType.OWNER) {
+            assert(permissions.contains(Permission.OWN));
+            assert(permissions.contains(Permission.AUTHORIZE));
+            assert(permissions.contains(Permission.DELETE));
+        } else {
+            assert(!permissions.contains(Permission.OWN));
+            assert(!permissions.contains(Permission.DELETE));
+            assert(!permissions.contains(Permission.AUTHORIZE));
+        }
+
+        if (roleType == RoleType.EDITOR || roleType == RoleType.OWNER) {
+            assert(permissions.contains(Permission.UPDATE));
+            assert(permissions.contains(Permission.READ));
+        } else {
+            assert(!permissions.contains(Permission.UPDATE));
+        }
+
+        if (roleType == RoleType.READER) {
+            assert(permissions.contains(Permission.READ));
+        }
+    }
+
     // TODO: add unit testing 
     public Set<Permission> getRawPermissions() {
+        return getRawPermissions(permissions);
+    }
+
+    private Set<Permission> getRawPermissions(Set<RolePermission> permissions) {
         return permissions
                 .stream()
                 .map(permission -> permission.getPermission())
