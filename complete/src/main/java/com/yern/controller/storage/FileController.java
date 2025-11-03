d86380848bcc50a8e2e9fb4be90dcf1af34c56a5
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-03 08:20:25 -0600

    adding userId to uploadAndSaveFile method

--- complete/src/main/java/com/yern/controller/storage/FileController.java
+++ complete/src/main/java/com/yern/controller/storage/FileController.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.yern.dto.authentication.UserDetailsImpl;
 import com.yern.dto.storage.UploadFileResponse;
 import com.yern.model.pattern.Section;
 import com.yern.model.storage.FileImpl;
@@ -32,12 +34,14 @@ public class FileController {
     // TODO: think about limits on file size and general file validation 
     @PostMapping("/section/upload")
     public ResponseEntity<UploadFileResponse> uploadFile(
-        @RequestParam("file") MultipartFile file
+        @RequestParam("file") MultipartFile file,
+        @AuthenticationPrincipal UserDetailsImpl currentUser
     ) throws UploadFileException, IOException {
         ResponseEntity<UploadFileResponse> resp;
 
         try {
-            FileImpl fileRecord = fileService.uploadFile(
+            FileImpl fileRecord = fileService.uploadAndSaveFile(
+                currentUser.getUserId(),
                 file,
                 Section.class.getSimpleName()
             );
