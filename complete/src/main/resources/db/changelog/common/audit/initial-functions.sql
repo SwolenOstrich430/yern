938833414fb6315c65869478893b5deffc1ec9f2
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-03 08:37:59 -0600

    fixing audit and created at timestamp functions

--- complete/src/main/resources/db/changelog/common/audit/initial-functions.sql
+++ complete/src/main/resources/db/changelog/common/audit/initial-functions.sql
@@ -4,7 +4,8 @@ declare
     curr_time timestamp without time zone;
 begin
     curr_time := now()::timestamp;
-    new.audit_timestamps = row(curr_time, curr_time);
+    new.created_at = curr_time;
+    new.updated_at = curr_time;
     
     return new;
 end;
@@ -13,7 +14,7 @@ $$ language plpgsql;
 create or replace function update_audit_timestamp()
 returns trigger as $$
 begin
-    new.audit_timestamps.updated_at = now()::timestamp;
+    new.updated_at = now()::timestamp;
     
     return new;
 end;
@@ -55,17 +56,6 @@ begin
 end;
 $$ language plpgsql;
 
-create or replace function set_audit_timestamp_triggers(
-    schema_name text,
-    table_name text
-)
-returns void as $$
-begin 
-    perform set_create_audit_timestamp_trigger(schema_name, table_name);
-    perform set_update_audit_timestamp_trigger(schema_name, table_name);
-end;
-$$ language plpgsql;
-
 create or replace function set_all_audit_timestamp_triggers(
     schema_name text
 )
@@ -75,17 +65,32 @@ declare
 begin 
     for result in (
         select 
-            table_name
+            table_name 
         from 
             information_schema.columns 
         where 
             table_schema = schema_name
         and 
-            udt_name = 'audit_timestamps'
+            column_name = 'created_at'
     ) loop
-        perform set_audit_timestamp_triggers(
-			schema_name, result.table_name
-		);
+        perform set_create_audit_timestamp_trigger(
+            schema_name, result.table_name
+        );
+    end loop;
+
+    for result in (
+        select 
+            table_name 
+        from 
+            information_schema.columns 
+        where 
+            table_schema = schema_name
+        and 
+            column_name = 'updated_at'
+    ) loop
+        perform set_update_audit_timestamp_trigger(
+            schema_name, result.table_name
+        );
     end loop;
 end;
 $$ language plpgsql;
