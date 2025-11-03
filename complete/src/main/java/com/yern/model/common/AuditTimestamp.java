21bfa47ab1ac07b02a2ae76bc43f3a9e2cb0c9f6
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-03 08:21:10 -0600

    adding audit timestamp class to consolidate createdAt and updatedAt fields

--- complete/src/main/java/com/yern/model/common/AuditTimestamp.java
+++ complete/src/main/java/com/yern/model/common/AuditTimestamp.java
@@ -1,16 +1,24 @@
 package com.yern.model.common;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-public class AuditTimestamp implements Serializable {
+@Embeddable
+public class AuditTimestamp {
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+    
+    @Column(name = "updated_at", nullable = false, updatable = true)
     private LocalDateTime updatedAt;
+
+    public AuditTimestamp() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
 }
