5a786d0d1d457a675bb1b573b3734e92316ef5ee
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-03 08:18:31 -0600

    fixing enum for postgres issues

--- complete/src/main/java/com/yern/model/security/authorization/RolePermission.java
+++ complete/src/main/java/com/yern/model/security/authorization/RolePermission.java
@@ -1,11 +1,16 @@
 package com.yern.model.security.authorization;
 
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,10 +21,17 @@ import lombok.Setter;
 @Setter 
 @NoArgsConstructor
 public class RolePermission {
+    public RolePermission(Role role, Permission permission) {
+        this.role = role; 
+        this.permission = permission;
+    }
+
     @Id 
     @ManyToOne
     private Role role;
 
-    @Enumerated(EnumType.ORDINAL)
+    @Column 
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
     private Permission permission;
 }
